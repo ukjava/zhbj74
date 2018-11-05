949e71529ca90f3829e568b9d6e96c3446e46497
ukjava <329602846@qq.com>
2018-11-05 22:35:27 +0800

    2次提交

---
+++ zhbj_1/app/src/main/java/com/example/administrator/zhbj74/fragment/ContentFragment.java
@@ -0,0 +1,22 @@
+package com.example.administrator.zhbj74.fragment;
+
+import android.view.View;
+
+import com.example.administrator.zhbj74.R;
+
+/**主页面Fragment
+ * Created by Administrator on 2018/11/5.
+ */
+
+public class ContentFragment extends BaseFragment {
+    @Override
+    public View initView() {
+        View view = View.inflate(mActivity, R.layout.fragment_content, null);
+        return view;
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
